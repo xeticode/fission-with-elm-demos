f65095eec1a9b3e2055b8258c2d46d64a30ceea3
darrow97 <>
2021-09-30 17:27:40 -0600

    added project for article 3.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import * as webnative from "webnative"
+import * as webnativeElm from "webnative-elm"
+import { Elm } from "/src/Main.elm"
+
+const app = Elm.Main.init({});
+
+app.ports.signOut.subscribe(() => {
+    webnative.leave({withoutRedirect:true});
+  });
+
+webnative.setup.debug({enabled:true});
+
+webnativeElm.setup({app, webnative});
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import elmPlugin from "vite-plugin-elm"
+
+export default {
+  // identify what plugins we want to use
+  plugins: [elmPlugin()],
+  // configure our build
+  build: {
+    // file path for the build output directory
+    outDir: "build",
+    target: "es2020"
+    // brotliSize: false
+  }
+}
